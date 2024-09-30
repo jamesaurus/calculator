70e06873024e5037680d0a7d494e82b48ec4afb2
James Ybanez <ybanezjames17@gmail.com>
2024-09-30 18:58:52 +1300

    Create operate function

--- script.js
+++ script.js
@@ -13,3 +13,25 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
     return num1/num2;
 }
+
+function operate(operator, num1, num2) {
+
+    switch(operator) {
+        case "add":
+            return add(num1, num2);
+        case "subtract":
+            return subtract(num1, num2);
+        case "multiply":
+            return multiply(num1, num2);
+        case "divide":
+            return divide(num1, num2);
+    }
+}
+
+/*
+console.log(operate("add", 1, 2));
+console.log(operate("subtract", 2, 1));
+console.log(operate("multiply", 3, 5));
+console.log(operate("divide", 10, 2));
+*/
+
