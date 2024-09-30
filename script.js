c3eac48ce537576a2e777ed285fc592895f6edc5
James Ybanez <ybanezjames17@gmail.com>
2024-09-30 23:57:48 +1300

    Add listeners to the buttons and partially implement calculator functionality

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 function add(num1, num2) {
-    return num1 + num2;
+    console.log(num1);
+    console.log(num2);
+    return Number(num1) + Number(num2);
 }
 
 function subtract(num1, num2) {  
@@ -17,13 +19,13 @@ function divide(num1, num2) {
 function operate(operator, num1, num2) {
 
     switch(operator) {
-        case "add":
+        case "+":
             return add(num1, num2);
-        case "subtract":
+        case "-":
             return subtract(num1, num2);
-        case "multiply":
+        case "*":
             return multiply(num1, num2);
-        case "divide":
+        case "/":
             return divide(num1, num2);
     }
 }
@@ -32,6 +34,36 @@ function changeDisplay(value) {
     const display = document.getElementById("display-text");
     display.innerHTML = value;
 }
+
+let operator = "";
+let num1 = "";
+let num2 = "";
+let displayValue = "0";
+
+// Add listeners to buttons
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+
+        // Check which button is pressed
+        if (button.className === "operator") {
+            operator = button.textContent;
+        } else if (button.className === "number") { 
+            if (operator === "") { // First number
+                num1 += button.textContent;
+                displayValue = num1;
+            } else { // Second number
+                num2 += button.textContent;
+                displayValue = num2;
+            }
+        } else if (button.className === "equals") {
+            displayValue = operate(operator, num1, num2); // if num1 = "" it is coerced into 0
+        }
+
+        // Update display
+        changeDisplay(displayValue);
+    })
+})
 
 /*
 console.log(operate("add", 1, 2));
